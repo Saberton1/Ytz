ae1a015f14ff3eedd20a37ebdaff8c06abb5bc41
Saberton1 <97967022+Saberton1@users.noreply.github.com>
2023-04-01 16:20:48 +0300

    Update Protected.lua

--- Protected.lua
+++ Protected.lua
@@ -2,7 +2,7 @@ repeat wait() until game:IsLoaded()
     wait(0.5)
     game.StarterGui:SetCore("SendNotification", {
     Title = "No more free trial";
-    Text = "Dm Dako to buy for a pika";
+    Text = "Dm Dako#4922 on discord to buy for a pika";
     Icon = "https://create.roblox.com/marketplace/asset/9583486354";
     Duration = 10;
     })
